298d64f932c2fdd7f4047e587af1a93d3cce63c3
Hasha Dar <43448050+hashadar@users.noreply.github.com>
2025-08-07 18:03:29 +0100

    retrigger

--- coinbase-aws/test_lambda_function.py
+++ coinbase-aws/test_lambda_function.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def lambda_handler(event, context):
     """
-    Simple test Lambda function to verify container deployment works.
+    Simple test Lambda function to verify container deployment works properly.
     """
     try:
         # Get environment variable
